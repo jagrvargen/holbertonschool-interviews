facb7e936f5f9107dccb1fed34cfa4c6451acea9
jagrvargen <jagarthewolf@gmail.com>
2018-06-11 23:11:55 +0000

    added file 2-binary_tree_is_full.c with header and readme updates

---
+++ 0x01-warm_up/2-binary_tree_is_full.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - Checks if a binary tree is full.
+ *
+ * @tree: A pointer to the root node of a binary tree.
+ *
+ * Return: An integer 1 if tree is full, 0 if not full or tree == NULL
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int search;
+
+	if (!tree)
+		return (0);
+
+	if ((tree->left && !tree->right) || (tree->right && !tree->left))
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (tree->right && tree->left)
+	{
+		search = binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right);
+		return (search);
+	}
+
+	return (0);
+}
--- 0x01-warm_up/README.md
+++ 0x01-warm_up/README.md
@@ -3,3 +3,5 @@
 0. Binary Search - A function in C that performs a binary search.
 
 1. Dictionary of List of Dictionaries - Sends an API request to record employee tasks and store them in a JSON file.
+
+2. Binary Tree is Full - A function in C that checks if a binary tree is full.
