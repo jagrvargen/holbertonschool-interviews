32988589726eac62e85adca6714bae36fb178d39
jagrvargen <jagarthewolf@gmail.com>
2018-06-11 16:35:31 +0000

    added file 1-dictionary_of_list_of_dictionaries.py with readme update

---
+++ 0x01-warm_up/1-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+   A Python script which retrieces employee data and stores it in a JSON file
+'''
+
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    url = "https://jsonplaceholder.typicode.com/users/"
+
+    user_dict = dict()
+
+    for i in range(1, 11):
+        u_url = url + str(i)
+        name = requests.get(u_url).json().get("name")
+        todos = requests.get(u_url + "/todos").json()
+        u_list = list()
+
+        for j in range(len(todos)):
+            u_list.append({"username": name, "task": todos[j].get("title"),
+                           "completed": todos[j].get("completed")})
+
+        user_dict[str(i)] = u_list
+
+    with open('todo_all_employees.json', 'w', encoding='utf-8') as f:
+        json.dump(user_dict, f)
--- 0x01-warm_up/README.md
+++ 0x01-warm_up/README.md
@@ -1,3 +1,5 @@
 # Warm Up Exercises
 
 0. Binary Search - A function in C that performs a binary search.
+
+1. Dictionary of List of Dictionaries - Sends an API request to record employee tasks and store them in a JSON file.
